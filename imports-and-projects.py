7d08088e64a8df171ab0b489a59a1e4a585432d0
seansenters <senters@stanford.edu>
2015-04-15 11:05:25 -0700

    Create imports-and-projects.py

---
+++ imports-and-projects.py
@@ -0,0 +1,7 @@
+('I am using Python:', '2.7.6 (default, Sep  9 2014, 15:04:36) \n[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.39)]')
+('I am making a new local directory named', 'data-hold/stuff')
+Traceback (most recent call last):
+  File "/Users/seansenters/Downloads/imports-and-setup.py", line 9, in <module>
+    os.makedirs(dirname, exist_ok = True)
+TypeError: makedirs() got an unexpected keyword argument 'exist_ok'
+[Finished in 0.1s with exit code 1]
