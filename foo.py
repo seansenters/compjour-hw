55694f8948e81c014cc8546d889ba76e10561622
seansenters <senters@stanford.edu>
2015-04-29 09:27:04 -0700

    Create foo.py

---
+++ foo.py
@@ -0,0 +1,15 @@
+def rain_map():
+
+Create a map of where it is (theoretically) raining (and people don't like it), from hashtag #rainraingoaway
+
+Arguments:  Tweets with hashtag, user information
+
+Return: Geolocation; longitude & latitude coordinates
+
+def map():
+
+Create JSON file with user coordinates
+
+Take geolocations, use link similar to JSON Quiz Part 2; basemap_url = 'https://maps.googleapis.com/maps/api/staticmap?zoom=1&size=500x400'
+
+Create markers, coords[0], coords[1], etc.
