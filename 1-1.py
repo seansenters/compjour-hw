9767e89749b903178016e4fa2f95247754d55490
seansenters <senters@stanford.edu>
2015-05-03 18:37:32 -0700

    Create 1-1.py

---
+++ 1-1.py
@@ -0,0 +1,14 @@
+import requests
+
+BASE_USAJOBS_URL = "https://data.usajobs.gov/api/jobs"
+
+state_name = 'New York'
+
+atts = {"CountrySubdivision":  state_name, 'NumberOfJobs':  1}
+
+resp = requests.get(BASE_USAJOBS_URL, params = atts)
+
+data = resp.json()
+
+print("%s has %s job listings." % (state_name, data['TotalJobs']))
+New York has 360 job listings.
