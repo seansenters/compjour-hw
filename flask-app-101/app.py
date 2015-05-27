a6278a01ecdefa560b6129ca6e1270dc284c022a
seansenters <senters@stanford.edu>
2015-05-27 08:50:37 -0700

    Update and rename app.py to flask-app-101/app.py

--- flask-app-101/app.py
+++ flask-app-101/app.py
@@ -4,31 +4,6 @@ from flask import render_template
 
 app = Flask(__name__)
 
-def get_csv():
-    csv_path = './static/la-riots-deaths.csv'
-    
-
-@app.route("/")
-def index():
-    template = 'index.html'
-    return render_template(template)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-    
-To exit: use 'exit', 'quit', or Ctrl-D.
-An exception has occurred, use %tb to see the full traceback.
-
-SystemExit: 1
-
-
-from flask import Flask
-
-from flask import render_template
-
-app = Flask(__name__)
-
 import csv
 
 def get_csv():
