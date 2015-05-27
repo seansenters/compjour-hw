88934de7f851d62c34184d2401bb415c84d87420
seansenters <senters@stanford.edu>
2015-05-27 09:01:16 -0700

    Create stanford-news-download.py

---
+++ opener-project/stanford-news-download.py
@@ -0,0 +1,14 @@
+import requests
+dirname = "data-hold/stuff"
+print("Downloading the news.stanford.edu homepage")
+response = requests.get("http://news.stanford.edu/")
+rawhtml = response.text
+print("news.stanford.edu has", len(rawhtml), "characters")
+print("Saving the current news.stanford.edu to my %s folder" % dirname)
+f = open(dirname + "/news.stanford.edu.html", "w")
+f.write(rawhtml)
+f.close()
+
+Downloading the news.stanford.edu homepage
+('news.stanford.edu has', 28128, 'characters')
+Saving the current news.stanford.edu to my data-hold/stuff folder
