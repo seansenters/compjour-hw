5b613cf9ab49e0cd2b574eb63e1ab083d53c70d0
seansenters <senters@stanford.edu>
2015-06-07 13:29:00 -0700

    Create 1.py

---
+++ search-script-scrape/1.py
@@ -0,0 +1,12 @@
+import bs4
+
+import requests
+
+response = requests.get('http://www.data.gov/')
+
+soup = bs4.BeautifulSoup(response.text)
+
+link = soup.select("small a")[0]
+
+print(link.text)
+131,971 datasets
