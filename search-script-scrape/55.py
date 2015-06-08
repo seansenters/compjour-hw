6e6ec78c2e8ceb1027f33aab2ab15c0836834898
seansenters <senters@stanford.edu>
2015-06-08 13:02:34 -0700

    Create 55.py

---
+++ search-script-scrape/55.py
@@ -0,0 +1,14 @@
+import bs4
+
+import requests
+
+data_url = "http://www.gao.gov/browse/topic"
+
+response = requests.get(data_url)
+
+soup = bs4.BeautifulSoup(response.text)
+
+link = soup.select("span")[30]
+
+print(link.text)
+(1,170)
