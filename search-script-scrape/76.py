1c39898e2b01ff6b643e15a00eeae76c9f39b4bd
seansenters <senters@stanford.edu>
2015-06-08 21:33:08 -0700

    Create 76.py

---
+++ search-script-scrape/76.py
@@ -0,0 +1,14 @@
+import bs4
+
+import requests
+
+data_url = "http://www.gao.gov/browse/topic"
+
+response = requests.get(data_url)
+
+soup = bs4.BeautifulSoup(response.text)
+
+link = soup.select("li")[16]
+
+print(link.text)
+All Topics (53,130)
