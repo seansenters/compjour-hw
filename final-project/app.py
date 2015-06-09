ab99845ceadaa9749a73f036155053a7d6ec3fa4
seansenters <senters@stanford.edu>
2015-06-09 16:41:27 -0700

    Update app.py

--- final-project/app.py
+++ final-project/app.py
@@ -15,6 +15,15 @@ def index():
     template = 'index.html'
     object_list = get_csv()
     return render_template(template, object_list=object_list)
+    
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
